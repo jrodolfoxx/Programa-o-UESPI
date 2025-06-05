5754afeda9a0554612a82b89f2a405234dd43776
Jose Rodolfo <167130885+jrodolfoxx@users.noreply.github.com>
2025-06-05 09:35:50 -0300

    Add files via upload

---
+++ P2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Olá, tudo bem?\n");
+    printf("Valor inteiro: %d.\n",10);
+    printf("Valor real: %f.\n",3.14159265);
+    printf("Valor real com duas casas: %2f.\n",3.14159265);
+    printf("Dados de texto: %c.\n", 'a');
+    printf("Sequencia de caracteres: %s.\n","texto");
+}
